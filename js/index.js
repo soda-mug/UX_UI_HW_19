edac6f3a592647a0abb3fb7b11c8347d4cead213
Chau Tran <chautran360@gmail.com>
2023-08-07 18:02:53 -0500

    click function added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 console.log("Your JS is working")
+function resume() {
+    window.open("https://docs.google.com/document/d/1i5Pl8ukrXrT_foZr_5Eetr78Lo0Alwk2PlmqBkSns-M/edit?usp=drive_link",'_blank')
+}
+function work1() {
+    window.open("https://docs.google.com/presentation/d/1XMDDg5jb3h5lAK3YQuY9DHsNt5gWOIWJ17oSQcXwnio/edit?usp=sharing",'_blank')
+}
+function work2() {
+    window.open("https://docs.google.com/document/d/19jr_Pl7mCEXphTXPtu-2aorMtbhG-iEYq9LCi-1yFlc/edit?usp=sharing",'_blank')
+}
+function work3() {
+    window.open("https://docs.google.com/document/d/1r4agRZAqbNaC7M6bR0-hRlXUe6qD2L8BaeU74OCYmuQ/edit?usp=sharing",'_blank')
+}
